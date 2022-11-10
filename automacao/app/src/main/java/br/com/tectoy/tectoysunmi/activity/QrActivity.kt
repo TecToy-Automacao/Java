f6060039b3cad8abe40f42db59585289589af40c
Adevan Neves Santos <78575445+adevan-neves-santos@users.noreply.github.com>
2022-11-10 14:54:46 -0400

    Retira ; desnecessário

--- automacao/app/src/main/java/br/com/tectoy/tectoysunmi/activity/QrActivity.kt
+++ automacao/app/src/main/java/br/com/tectoy/tectoysunmi/activity/QrActivity.kt
@@ -1,22 +1,22 @@
 package br.com.tectoy.tectoysunmi.activity
 
-import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.hardware.usb.UsbDevice;
-import android.hardware.usb.UsbManager;
-import android.os.Bundle;
-import android.os.IBinder;
-import android.text.TextUtils;
-import android.util.DisplayMetrics;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.app.Activity
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.hardware.usb.UsbDevice
+import android.hardware.usb.UsbManager
+import android.os.Bundle
+import android.os.IBinder
+import android.text.TextUtils
+import android.util.DisplayMetrics
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 
 import androidx.annotation.Nullable;
 
