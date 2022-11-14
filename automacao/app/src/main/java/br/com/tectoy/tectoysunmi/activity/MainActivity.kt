20b55616b28e2da86719240c4f998a359da6bb5e
Adevan Neves Santos <78575445+adevan-neves-santos@users.noreply.github.com>
2022-11-14 13:15:56 -0400

    Finaliza classe DemoDetails e outras funções

--- automacao/app/src/main/java/br/com/tectoy/tectoysunmi/activity/MainActivity.kt
+++ automacao/app/src/main/java/br/com/tectoy/tectoysunmi/activity/MainActivity.kt
@@ -14,11 +14,7 @@ import android.graphics.Matrix
 import android.hardware.usb.UsbDevice
 import android.hardware.usb.UsbManager
 import android.media.MediaRouter
-import android.os.Build
-import android.os.Bundle
-import android.os.Environment
-import android.os.IBinder
-import android.os.RemoteException
+import android.os.*
 import android.text.TextUtils
 import android.util.DisplayMetrics
 import android.view.Display
@@ -38,13 +34,14 @@ import com.sunmi.extprinterservice.ExtPrinterService
 import java.util.HashMap
 import br.com.tectoy.tectoysunmi.R
 import br.com.tectoy.tectoysunmi.activity.ExemploNFCIdRW.NfcExemplo
+import br.com.tectoy.tectoysunmi.databinding.ActivityMainBinding
 import br.com.tectoy.tectoysunmi.threadhelp.ThreadPoolManageer
 import br.com.tectoy.tectoysunmi.utils.KTectoySunmiPrinter
 import br.com.tectoy.tectoysunmi.utils.TectoySunmiPrint
 import sunmi.sunmiui.dialog.DialogCreater
 import sunmi.sunmiui.dialog.HintOneBtnDialog
 
-class MainActivity : AppCompatActivity(){
+open class MainActivity : AppCompatActivity(){
     var height = 0
     lateinit var mHintOneBtnDialog:HintOneBtnDialog
     var run:Boolean = false
@@ -74,18 +71,132 @@ class MainActivity : AppCompatActivity(){
                                 DemoDetails(R.string.function_m_Sitef, R.drawable.function_payment, MSitef::class.java),
                                 DemoDetails(R.string.display, R.drawable.telas, DisplayActivity::class.java)
     )
+    private var videoDisplay:VideoDisplay? = null
+    private var screenManager:ScreenManager = ScreenManager.getInstance()
 
-    private class DemoDetails{
-        @StringRes   private val titleId:Int
-        @DrawableRes private val iconResID:Int
-        private val activityClass:Activity?
-        constructor(@StringRes titleId:Int,
-                    @DrawableRes descriptionId:Int,
-                    activityClass:Activity?){
-            this.titleId       = titleId
-            this.iconResID     = descriptionId
-            this.activityClass = activityClass
+    private lateinit var binding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupRecyclerView()
+        val dm = DisplayMetrics()
+        window.windowManager.defaultDisplay.getMetrics(dm)
+        val width:Int = dm.widthPixels   //Largura da Tela
+        height = dm.heightPixels  //Altura da tela
+        isVertical =  height > width
+        isK1 = isHaveCamera() && isVertical
+        var deviceName:String = getDeviceName()
+        if(isK1 && (height > 1856)){
+            connectKPrintService()
         }
+        val mediaRouter:MediaRouter = this.getSystemService(Context.MEDIA_ROUTER_SERVICE) as MediaRouter
+        val route:MediaRouter.RouteInfo? = mediaRouter.getSelectedRoute(1)
+
+        if (route!=null){
+            val presentationDisplay:Display? = route.presentationDisplay
+            if(presentationDisplay != null){
+                val presentation:Presentation = VideoDisplay(this, presentationDisplay, Environment.getExternalStorageDirectory().path+"/video_01.mp4")
+                presentation.show()
+            }
+        }
+    }
+    // Conexão Impressão K2
+    private fun connectKPrintService() {
+        val intent:Intent = Intent()
+        intent.`package` = "com.sunmi.extprinterservice"
+        intent.action = "com.sunmi.extprinterservice.PrinterService"
+        bindService(intent,connService, Context.BIND_AUTO_CREATE)
+    }
+    private var connService:ServiceConnection = object: ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            extPrinterService = ExtPrinterService.Stub.asInterface(service)
+            kPrinterPresenter = KTectoySunmiPrinter(this@MainActivity, extPrinterService)
+        }
+        override fun onServiceDisconnected(name: ComponentName?) {
+            extPrinterService = null
+        }
+    }
+
+    private fun getDeviceName():String{
+        val manufacturer:String = Build.MANUFACTURER
+        val model:String = Build.MODEL
+        if (model.startsWith(manufacturer)){
+            return capitalize(model)
+        }
+        return "${capitalize(manufacturer)} $model"
+    }
+
+    private fun capitalize(str:String):String{
+        if (TextUtils.isEmpty(str)) {
+            return str
+        }
+        var arr:CharArray = str.toCharArray()
+        var capitalizeNext:Boolean = true
+        var phrase:StringBuilder = StringBuilder()
+        for(c:Char in arr){
+            if (capitalizeNext && Character.isLetter(c)){
+                phrase.append(Character.toUpperCase(c))
+                capitalizeNext = false
+                continue
+            } else if(Character.isWhitespace(c)){
+                capitalizeNext = true
+            }
+            phrase.toString()
+        }
+        return phrase.toString()
+    }
+
+    private fun isHaveCamera() : Boolean{
+        val deviceHashMap : HashMap<String, UsbDevice> = (getSystemService(Activity.USB_SERVICE) as UsbManager).deviceList
+        for(entry in deviceHashMap.entries){
+            var usbDevice = entry.value
+            if(!TextUtils.isEmpty(usbDevice.deviceName) && usbDevice.deviceName == "Orb"){
+                return true
+            }
+            if(!TextUtils.isEmpty(usbDevice.deviceName) && usbDevice.deviceName == "Astra"){
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun setupRecyclerView() {
+        val layoutManage:GridLayoutManager = GridLayoutManager(this,2)
+        var mRecyclerView:RecyclerView = binding.worklist
+        mRecyclerView.layoutManager = layoutManage
+        mRecyclerView.adapter= WorkTogetherAdapter()
+    }
+
+    inner class WorkTogetherAdapter: RecyclerView.Adapter<WorkTogetherAdapter.MyViewHolder>() {
+        inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            lateinit var tv:TextView
+            lateinit var demoDetails:DemoDetails
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+            val v:View = LayoutInflater.from(parent.context).inflate(R.layout.work_item, parent,false)
+            return MyViewHolder(v)
+        }
+
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+            holder.demoDetails = demos[position]
+            holder.tv.text = demos[position].titleId // dúvida sobre tipo e referência
+        }
+
+        override fun getItemCount(): Int {
+            TODO("Not yet implemented")
+        }
+    }
+    //https://medium.com/android-dev-br/generics-e-variance-em-kotlin-in-out-t-ca5ca07c9fc5
+    class DemoDetails(
+        @StringRes   val titleId: Int,
+        @DrawableRes val descriptionId: Int,
+        val activityClass: Class<out Activity>?
+    ) {
+        @DrawableRes val iconResID:Int = descriptionId
+        var empty = Array<String>(3) { "it = $it" }
     }
 
 }
